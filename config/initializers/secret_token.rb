b2da105515b6c5256e264fd77c2c3c02db15ae22
Eliah Hecht <eliahhecht@gmail.com>
2010-12-16 18:45:46 -0500

    Scaffolded up a bare-bones Things

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFavoriteThings::Application.config.secret_token = '3df86a00f3b865ce728c8dbf00c058cd00da176b6458a965585c9ccf9c19a270e844a961da700eb88c04e4696d4a381bd3e522dc1c216a3aff33a1e004e3a695'
